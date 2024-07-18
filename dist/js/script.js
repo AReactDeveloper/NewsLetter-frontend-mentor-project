cc86635038f3f6eda61730f01dcdf1292bf6945b
abdelhak el ahrach <winston.abdelhak@gmail.com>
2024-07-18 14:56:15 +0100

    all

---
+++ dist/js/script.js
@@ -0,0 +1,31 @@
+const submitBtn = document.querySelector('.button')
+const email = document.getElementById('email')
+const errorLabel = document.querySelector('.form-group-labels-errorLabel')
+const dismisbtn = document.getElementById('dismissbtn')
+
+
+submitBtn.addEventListener('click',e=>{
+    e.preventDefault()
+    console.log(errorLabel)
+    if(isValidEmail(email.value)){
+        email.classList.remove('error-input')
+        errorLabel.style.display = 'none'
+        window.location.href = '/sucess.html';
+
+    }else{
+        email.classList.add('error-input')
+        errorLabel.style.display = 'block'
+    }
+})
+
+
+dismisbtn.addEventListener('click',e=>{
+    e.preventDefault()
+    window.location.href = '/'
+})
+
+function isValidEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  }
+
